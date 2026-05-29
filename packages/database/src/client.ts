2d3b324ce0f33fd528d72b2b1030efd8f20ebd89
MinKhantAung23 <minkhantaung743@gmail.com>
2026-05-29 13:09:25 +0630

    add prisma db

---
+++ packages/database/src/client.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+
+import { PrismaClient } from "./generated/prisma";
+
+const globalForPrisma = globalThis as {
+  prisma?: PrismaClient;
+};
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
